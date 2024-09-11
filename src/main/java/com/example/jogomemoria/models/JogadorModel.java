9f4ec66c748c55ac7da79449f683dc517798643c
Rayan Junio <rayanjunior234@gmail.com>
2024-09-11 10:01:13 -0300

    adiciona propriedades para auxiliar na persistencia de um jogador em um arquivo

--- src/main/java/com/example/jogomemoria/models/JogadorModel.java
+++ src/main/java/com/example/jogomemoria/models/JogadorModel.java
@@ -1,13 +1,20 @@
 package com.example.jogomemoria.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class JogadorModel {
+
+  @JsonProperty("nome")
   private String nome;
+  @JsonProperty("pontos")
   private int pontos;
 
   public JogadorModel(String nome) {
     this.nome = nome;
     this.pontos = 0;
   }
+
+  public JogadorModel() {}
 
   public String getNome() {
     return nome;
@@ -23,5 +30,17 @@ public class JogadorModel {
 
   public void setPontos(int pontos) {
     this.pontos = pontos;
+  }
+
+  public void adicionarPontuacao() {
+    this.pontos++;
+  }
+
+  @Override
+  public String toString() {
+    return "JogadorModel{" +
+            "nome='" + nome + '\'' +
+            ", pontos=" + pontos +
+            '}';
   }
 }
