ad46a4f3af41e4891da2d8e568f80f72b4b428bf
Rayan Junio <rayanjunior234@gmail.com>
2024-09-11 10:20:01 -0300

    Adiciona o service de jogadores para salvá-los no arquivo

--- src/main/java/com/example/jogomemoria/models/JogadorModel.java
+++ src/main/java/com/example/jogomemoria/models/JogadorModel.java
@@ -20,16 +20,8 @@ public class JogadorModel {
     return nome;
   }
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-
   public int getPontos() {
     return pontos;
-  }
-
-  public void setPontos(int pontos) {
-    this.pontos = pontos;
   }
 
   public void adicionarPontuacao() {
